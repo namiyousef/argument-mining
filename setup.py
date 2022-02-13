5524cbf9265b25824d28c29d544670429ab435aa
Yousef <namiyousef@hotmail.com>
2022-02-13 18:30:03 +0000

    adds setup file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ArgMiner',
+    version='0.0.1',
+    description='',
+    author='Yousef Nami',
+    author_email='namiyousef@hotmail.com',
+    url='https://github.com/namiyousef/argument-mining',
+    #install_requires=[],
+    #package_data={}
+    packages=find_packages(exclude=('tests*', 'experiments*')),
+    license='MIT',
+    #entry_points=(),
+)
