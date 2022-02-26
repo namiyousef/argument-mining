0aa8802a7ec51ad8b69be856946bb3d7abb0d6d9
Yousef <namiyousef@hotmail.com>
2022-02-26 12:04:34 +0000

    adds utils file

---
+++ argminer/utils.py
@@ -0,0 +1,4 @@
+def kaggle_split():
+    """Function to split discourse text as done by Kaggle. See https://www.kaggle.com/c/feedback-prize-2021/discussion/297591 for full details
+    :return:
+    """
