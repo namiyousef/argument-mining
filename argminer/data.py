d5f7012121fe54ac9cb602759de4dace99f0f443
Yousef <namiyousef@hotmail.com>
2022-03-04 18:11:02 +0000

    #5 migrates dataset to argminer

---
+++ argminer/data.py
@@ -0,0 +1,54 @@
+from torch.utils.data import Dataset
+import torch
+from pandas.api.types import is_string_dtype
+
+class KaggleDataset(Dataset):
+    """
+    Class for loading data in batches after it has been processed
+    """
+    def __init__(self, dataframe, tokenizer, max_length):
+
+        super().__init__()
+
+        # -- prepare data
+        assert sorted(dataframe.columns) == ['labels', 'text'], f"Please make sure input dataframe has the columns (text, labels)"
+        # data must be in the correct format
+        self.inputs = dataframe.text.values
+        self.targets = dataframe.labels.values
+        if not is_string_dtype(self.inputs): raise TypeError('Text data must be string type')
+        # TODO assertion below is bug; not deleting so remember to add correct assertions
+        #if not is_integer_dtype(self.targets): raise TypeError('Label data must be integer type')
+
+        # -- prepare tokenizer
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+
+    def __len__(self):
+        return len(self.inputs)
+
+    def __getitem__(self, index):
+        # self.inputs anf self.targets must be of a type that is indexible as shown
+        inputs = self.inputs[index]
+        targets = self.targets[index]
+
+        inputs = self.tokenizer(
+            # consider parametrising these
+            inputs.split(),
+            is_split_into_words=True, # this means that extra \n should be ignored
+            padding='max_length',
+            truncation=True,
+            max_length=self.max_length
+        )
+
+        word_ids = inputs.word_ids()
+        word_id_mask = [bool(word_id) for word_id in word_ids]
+        word_ids = [word_id for word_id in word_ids if word_id]
+
+        inputs = {
+            key: torch.as_tensor(val, dtype=torch.long) for key, val in inputs.items()
+        }
+        targets = torch.as_tensor(targets, dtype=torch.long)
+        expanded_targets = torch.zeros(self.max_length, dtype=torch.long)
+        expanded_targets[word_id_mask] = targets[word_ids]
+
+        return (inputs, expanded_targets)
