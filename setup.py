185441f86d86b330ee934d69332405fa75535ff6
Yousef <namiyousef@hotmail.com>
2022-03-07 21:45:40 +0000

    updates package requirements

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author='Yousef Nami',
     author_email='namiyousef@hotmail.com',
     url='https://github.com/namiyousef/argument-mining',
-    #install_requires=[],
+    install_requires=['torch', 'transformers', 'sentencepiece'],
     #package_data={}
     packages=find_packages(exclude=('tests*', 'experiments*')),
     license='MIT',
