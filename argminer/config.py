d500a8fec9743924bff9464627290e19320b1c39
Yousef <namiyousef@hotmail.com>
2022-03-13 18:38:21 +0000

    adds configuration for sending emails

---
+++ argminer/config.py
@@ -0,0 +1,6 @@
+import os
+
+# -- get email details
+EMAIL = os.environ.get('EMAIL', 'nlp.fyp1800@gmail.com')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD', 'password')
+EMAIL_RECIPIENTS = os.environ.get('EMAIL_RECIPIENTS', EMAIL)
