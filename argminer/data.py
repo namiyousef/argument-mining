a4dc13a9ee7e74262f9f7834dac0495b098be8e8
Yousef <namiyousef@hotmail.com>
2022-03-14 20:30:14 +0000

    adds files for training

--- argminer/data.py
+++ argminer/data.py
@@ -52,3 +52,70 @@ class KaggleDataset(Dataset):
         expanded_targets[word_id_mask] = targets[word_ids]
 
         return (inputs, expanded_targets)
+
+
+LABEL_ALL_SUBTOKENS = True
+output_labels = ['O', 'B-Lead', 'I-Lead', 'B-Position', 'I-Position', 'B-Claim', 'I-Claim', 'B-Counterclaim',
+                 'I-Counterclaim',
+                 'B-Rebuttal', 'I-Rebuttal', 'B-Evidence', 'I-Evidence', 'B-Concluding Statement',
+                 'I-Concluding Statement']
+
+labels_to_ids = {v: k for k, v in enumerate(output_labels)}
+
+class BigBirdDataset(Dataset):
+    """Dataset from the bigbird notebook (modified)
+    """
+
+
+    def __init__(self, dataframe, tokenizer, max_len, get_wids):
+        self.len = len(dataframe)
+        self.data = dataframe
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+        self.get_wids = get_wids  # for validation
+
+    def __getitem__(self, index):
+        # GET TEXT AND WORD LABELS
+        text = self.data.text[index]
+        word_labels = self.data.entities[index] if not self.get_wids else None
+
+        # TOKENIZE TEXT
+        encoding = self.tokenizer(text.split(),
+                                  is_split_into_words=True,
+                                  # return_offsets_mapping=True,
+                                  padding='max_length',
+                                  truncation=True,
+                                  max_length=self.max_len)
+        word_ids = encoding.word_ids()
+
+        # CREATE TARGETS
+        if not self.get_wids:
+            previous_word_idx = None
+            label_ids = []
+            for word_idx in word_ids:
+                if word_idx is None:
+                    label_ids.append(-100)
+                elif word_idx != previous_word_idx:
+                    label_ids.append(labels_to_ids[word_labels[word_idx]])
+                else:
+                    if LABEL_ALL_SUBTOKENS:
+                        label_ids.append(labels_to_ids[word_labels[word_idx]])
+                    else:
+                        label_ids.append(-100)
+                previous_word_idx = word_idx
+            encoding['labels'] = label_ids
+
+        # CONVERT TO TORCH TENSORS
+        item = {key: torch.as_tensor(val, dtype=torch.long) for key, val in encoding.items()}
+        if self.get_wids:
+            word_ids2 = [w if w is not None else -1 for w in word_ids]
+            item['wids'] = torch.as_tensor(word_ids2)
+
+            return item
+        else:
+            targets = encoding.pop('labels')
+            inputs = encoding
+            return inputs, targets
+
+    def __len__(self):
+        return self.len
--- argminer/utils.py
+++ argminer/utils.py
@@ -2,7 +2,8 @@ from argminer.config import EMAIL, EMAIL_PASSWORD, EMAIL_RECIPIENTS
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-
+import base64
+import torch
 
 def kaggle_split():
     """Function to split discourse text as done by Kaggle. See https://www.kaggle.com/c/feedback-prize-2021/discussion/297591 for full details
@@ -13,7 +14,6 @@ def send_job_completion_report(job_name):
 
     SENDER = EMAIL
     SENDER_PASSWORD = EMAIL_PASSWORD
-    print(EMAIL_PASSWORD)
     RECIEVER = EMAIL_RECIPIENTS
 
 
@@ -39,3 +39,25 @@ def send_job_completion_report(job_name):
     session.sendmail(SENDER, RECIEVER, text)
     session.quit()
     print('Mail successfully sent.')
+
+def encode_model_name(model_name, epoch):
+    model_name = f'{model_name}_{epoch}'
+    model_name_b = model_name.encode('ascii')
+    encoded_model_name = base64.b64encode(model_name_b).decode('ascii')
+    return encoded_model_name
+
+def decode_model_name(encoded_model_name):
+    encoded_model_name_b = encoded_model_name.encode('ascii')
+    model_name = base64.b64decode(encoded_model_name_b).decode('ascii')
+    return model_name
+
+def _move(data, device='cpu'):
+    if torch.is_tensor(data):
+        return data.to(device)
+    elif isinstance(data, dict):
+        return {key: tensor.to(device) for key, tensor in data.items()}
+    elif isinstance(data, list):
+        raise NotImplementedError('Currently no support for tensors stored in lists.')
+    else:
+        raise TypeError('Invalid data type.')
+
