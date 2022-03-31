9223aaa663a65bdaa04452f95bfdeef81d61a9a0
Yousef <namiyousef@hotmail.com>
2022-03-31 03:39:09 +0200

    updates requirements

--- setup.py
+++ setup.py
@@ -17,7 +17,9 @@ setup(
         'tqdm',
         'sklearn',
         'protobuf',
-        'colabtools@git+https://git@github.com/namiyousef/colab-utils.git'
+        'colabtools@git+https://git@github.com/namiyousef/colab-utils.git',
+        'mlutils@git+https://git@github.com/namiyousef/ml-utils.git'
+        #'pip install torch==1.7.1+cu101 torchvision==0.8.2+cu101 torchaudio==0.7.2 -f https://download.pytorch.org/whl/torch_stable.html'
     ],
     #package_data={}
     packages=find_packages(exclude=('tests*', 'experiments*')),
