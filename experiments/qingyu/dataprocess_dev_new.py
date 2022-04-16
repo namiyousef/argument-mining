67f99ad50dc2e42a63a7537d916ce1e5baa32c25
Valerie_Feng <44435727+ValerieF412@users.noreply.github.com>
2022-04-16 17:15:02 +0800

    Fixed Bugs in Persuade DataProcessor

--- experiments/qingyu/dataprocess_dev_new.py
+++ experiments/qingyu/dataprocess_dev_new.py
@@ -20,6 +20,7 @@ from argminer.evaluation import inference
 
 
 #NEW VERSION
+
 class PersuadeProcessor(DataProcessor):
     def __init__(self,path=''):
         super().__init__(path)
@@ -30,50 +31,67 @@ class PersuadeProcessor(DataProcessor):
         
         df_ground_truth = pd.read_csv(path_to_ground_truth)
         df_texts = df_from_text_files(path_to_text_dir)
-        
-        
         df_ground_truth = df_ground_truth.sort_values(['id', 'discourse_start', 'discourse_end'])
         df_ground_truth = df_ground_truth.drop(columns=['discourse_id','discourse_start','discourse_end','discourse_type_num'])
         
+        ##Rename columns
+        df_ground_truth = df_ground_truth.rename(columns={'discourse_type':'label','predictionstring':'predictionString',
+                                          'discourse_text':'text','id':'doc_id'})
+        df_texts = df_texts.rename(columns={'text':'doc_text','id':'doc_id'})
         
-        df_texts['text_split'] = df_texts.text.str.split()
+        df_texts['text_split'] = df_texts.doc_text.str.split()
         df_texts['range'] = df_texts['text_split'].apply(lambda x: list(range(len(x))))
         df_texts['start_id'] = df_texts['range'].apply(lambda x: x[0])
         df_texts['end_id'] = df_texts['range'].apply(lambda x: x[-1])
         df_texts = df_texts.drop(columns=['text_split','range'])
         
         
-        df = df_ground_truth.groupby('id').agg({
-            'discourse_text':lambda x: ' '.join(x),
-            'predictionstring': lambda x: ' '.join(x),
+        df = df_ground_truth.groupby('doc_id').agg({
+            'text':lambda x: ' '.join(x),
+            'predictionString': lambda x: ' '.join(x),
     
         }).reset_index()
         
         df = df.merge(df_texts)
         
-        df['predictionstring'] = df.predictionstring.apply(lambda x: [int(num) for num in x.split()])
-        df['pred_str_start_id'] = df.predictionstring.apply(lambda x: x[0])
-        df['pred_str_end_id'] =  df.predictionstring.apply(lambda x: x[-1])
+        df['predictionString'] = df.predictionString.apply(lambda x: [int(num) for num in x.split()])
+        df['pred_str_start_id'] = df.predictionString.apply(lambda x: x[0])
+        df['pred_str_end_id'] =  df.predictionString.apply(lambda x: x[-1])
         
-        new_df = pd.DataFrame()
-        for row in df.sort_values('id').itertuples(index=False):
+        new_df_end = pd.DataFrame()
+        new_df_start = pd.DataFrame()
+        for row in df.sort_values('doc_id').itertuples(index=False):
             if row.end_id != row.pred_str_end_id:
-                s = row.text.split()[row.pred_str_end_id:]
+                s = row.doc_text.split()[row.pred_str_end_id+1:]
                 new_string = ' '.join(s)
                 new_predsStr = list(range(row.pred_str_end_id+1,row.end_id+1))
                 new_predsStr = " ".join([str(i) for i in new_predsStr])
                 new_type = 'Other'
-                new_id = row.id
-                new_row = {'id': new_id, 'discourse_text':new_string ,'discourse_type':'Other','predictionstring':new_predsStr}
-                new_df = new_df.append(new_row,ignore_index=True)
-                
-        df_combined = df_ground_truth.append(new_df,ignore_index=True)
+                new_id = row.doc_id
+                new_row = {'doc_id': new_id, 'text':new_string ,'label':'Other','predictionString':new_predsStr}
+                new_df_end = new_df_end.append(new_row,ignore_index=True)
+
+            if row.start_id != row.pred_str_start_id:
+                s = row.doc_text.split()[:row.pred_str_start_id]
+                new_string = ' '.join(s)
+                new_predsStr = list(range(row.start_id,row.pred_str_start_id))
+                new_predsStr = " ".join([str(i) for i in new_predsStr])
+                new_type = 'Other'
+                new_id = row.doc_id
+                new_row = {'doc_id': new_id, 'text':new_string ,'label':'Other','predictionString':new_predsStr}
+                new_df_start = new_df_start.append(new_row,ignore_index=True)
+
+
+        df_combined = df_ground_truth.append(new_df_end,ignore_index=True)
+        df_combined = new_df_start.append(df_combined,ignore_index=True)
+
         
+
         ##KEEP THE FOLLOWING OR NOT???
-        df_combined.predictionstring = df_combined.predictionstring.apply(lambda x: [int(num) for num in x.split()])
-        df_combined['start'] = df_combined.predictionstring.apply(lambda x: x[0])
-        df_combined['end'] =  df_combined.predictionstring.apply(lambda x: x[-1])
-        df_combined= df_combined.sort_values(['id', 'start', 'end'])
+        df_combined.predictionString = df_combined.predictionString.apply(lambda x: [int(num) for num in x.split()])
+        df_combined['start'] = df_combined.predictionString.apply(lambda x: x[0])
+        df_combined['end'] =  df_combined.predictionString.apply(lambda x: x[-1])
+        df_combined= df_combined.sort_values(['doc_id', 'start', 'end'])
 
         
         self.dataframe = df_combined
@@ -87,7 +105,7 @@ class PersuadeProcessor(DataProcessor):
         df = self.dataframe.copy()
 
         for processor in processors:
-            df['discourse_text'] = df['discourse_text'].apply(processor)
+            df['text'] = df['text'].apply(processor)
 
 
         # add labelling strategy
@@ -96,9 +114,9 @@ class PersuadeProcessor(DataProcessor):
             add_beg='b' in strategy
         )
         
-        df['discourse_type'] = df[['discourse_type', 'predictionstring']].apply(
+        df['label'] = df[['label', 'predictionString']].apply(
             lambda x: _generate_entity_labels(
-                len(x['predictionstring']), x['discourse_type'], **label_strat
+                len(x['predictionString']), x['label'], **label_strat
             ), axis=1
         )
 
@@ -110,26 +128,25 @@ class PersuadeProcessor(DataProcessor):
     
     def _postprocess(self):
         df_post = self.dataframe.copy()
-        df_post['predictionstring'] = df_post['predictionstring'].apply(
+        df_post['predictionString'] = df_post['predictionString'].apply(
                     lambda x: ' '.join([str(item) for item in x])
                 )
 
-        df_post['discourse_type'] = df_post['discourse_type'].apply(
+        df_post['label'] = df_post['label'].apply(
                     lambda x: ' '.join([str(item) for item in x])
                 )
-        df_post = df_post.groupby('id').agg({
-            'discourse_text':lambda x: ' '.join(x),
-            'predictionstring': lambda x: ' '.join(x),
-            'discourse_type': lambda x: ' '.join(x)
+        df_post = df_post.groupby('doc_id').agg({
+            'text':lambda x: ' '.join(x),
+            'predictionString': lambda x: ' '.join(x),
+            'label': lambda x: ' '.join(x)
         })
-        df_post['discourse_type'] = df_post['discourse_type'].str.split()
-        df_post['predictionstring'] = df_post['predictionstring'].str.split().apply(lambda x: [int(x) for x in x])
+        df_post['label'] = df_post['label'].str.split()
+        df_post['predictionString'] = df_post['predictionString'].str.split().apply(lambda x: [int(x) for x in x])
 
         df_post = df_post.reset_index()
         
-        df_post = df_post.rename(columns={'discourse_type':'labels','predictionstring':'predictionString',
-                                          'discourse_text':'text','id':'doc_id'})
-        self.dataframe = df
+    
+        self.dataframe = df_post
         self.status = 'postprocessed'
 
         return self
@@ -151,4 +168,3 @@ def _generate_entity_labels(length, label, add_end=False, add_beg=True):
             labels[0] = f'B-{label}'
 
     return labels
-
